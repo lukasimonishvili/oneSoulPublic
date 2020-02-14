7dc703817c4076fdd1d6096931215b6cb55eb878
lukasimonishvili <simonishvililuka@gmail.com>
2020-02-14 19:09:10 +0400

    responsive

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"]
 })
 export class HeaderComponent implements OnInit {
+  private hi: boolean = JSON.parse(window.localStorage.getItem("hi")) || false;
+  private hiCount: number;
+  private isNavigationOpen: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.hiCount = 1;
   }
 
+  sayHi() {
+    if (!this.hi) {
+      this.hiCount = this.hiCount + 1;
+      window.localStorage.setItem("hi", JSON.stringify(true));
+      this.hi = JSON.parse(window.localStorage.getItem("hi"));
+    }
+  }
+
+  toggleNavigation() {
+    this.isNavigationOpen = !this.isNavigationOpen;
+  }
 }
